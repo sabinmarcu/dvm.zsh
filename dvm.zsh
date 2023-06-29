013776f1829c6e674faed50d835a1673ab563e47
Sabin Marcu <sabinmarcu@gmail.com>
2023-06-29 04:30:14 +0300

    fix: reintroduce return statements

--- dvm.zsh
+++ dvm.zsh
@@ -8,15 +8,18 @@ local exe="$dvm_bin_dir/dvm"
 
 [[ -z $DVM_DIR ]] && export DVM_DIR=${dvm_dir}
 
-if [[ ! -f $exe ]]; then 
+if [[ ! -f $exe ]]; then
   echo "\033[34mInstalling DVM\033[0m"
-  if curl -fsSL https://dvm.deno.dev | sh; then 
+  if curl -fsSL https://dvm.deno.dev | sh; then
     echo "\033[32mDone!\033[0m"
   else
     echo "\033[31mThere was an error installing DVM!\033[0m"
+    false
   fi
 fi
 
 if [[ -f $exe ]]; then
   export PATH="$dvm_dir:$PATH"
 fi
+
+true
