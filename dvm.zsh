55d94548180c4c269536930fb42c2b7add2a39a5
Sabin Marcu <sabinmarcu@gmail.com>
2023-06-29 05:00:35 +0300

    fix: properly add completions

--- dvm.zsh
+++ dvm.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+local ZSH_DVM_DIR=${0:a:h}
+
 local dvm_dir="${DVM_DIR:-$HOME/.dvm}"
 local dvm_bin_dir="$dvm_dir/bin"
 local exe="$dvm_bin_dir/dvm"
@@ -16,9 +18,18 @@ if [[ ! -f $exe ]]; then
   fi
 fi
 
+local comp_dir="$ZSH_DVM_DIR/completions"
+local comp_file="$comp_dir/_dvm"
+
 if [[ -f $exe ]]; then
   export PATH="$dvm_dir:$PATH"
-  eval "$(dvm completions zsh)"
+  if [[ ! -f $comp_file ]]; then
+    mkdir -p $comp_dir
+    if ! ((${fpath[(Ie)$comp_dir]})); then
+      fpath=($comp_dir $fpath)
+    fi
+    dvm completions zsh > $comp_file
+  fi
 fi
 
 true
